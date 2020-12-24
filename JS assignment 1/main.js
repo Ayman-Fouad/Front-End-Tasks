8c4f819cb6417f8a81f96b26061d8bd93a7a280b
Ayman-Fouad <ayman_fouad@outlook.com>
2020-12-24 18:39:48 +0200

    Js first assignment

---
+++ JS assignment 1/main.js
@@ -0,0 +1,32 @@
+// Assignment 1
+
+let radius = 5;
+let area = Math.PI * radius * radius
+console.log(`The area of a circle is ${area}`)
+let circumference = 2 * Math.PI * radius
+console.log(`The circumference is ${circumference}`)
+
+// Assignment 2
+
+let numb = prompt("Enter a number");
+if (numb % 2 == 0){
+console.log("The number is even");
+}
+else{
+    console.log("The number is odd");
+}
+
+// Assignment 3
+
+console.log("Hello World!")
+
+if(true){
+    let x = 10;
+}
+console.log(x);
+
+/* This page has perfectly good code from the begining, untill last code is wrong.
+when running this page it interprets each line from the begining untill line number (26)
+where the error exists.
+If javascript was compiled it would't run from the begining and just give error. 
+*/
