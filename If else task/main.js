aed4a7f421ee847cfe15acd6e3042011cc9af5b8
Ayman-Fouad <ayman_fouad@outlook.com>
2020-12-25 14:53:00 +0200

    in-course js task

---
+++ If else task/main.js
@@ -0,0 +1,15 @@
+// Assignment 1
+let numb = prompt("Enter a number");
+if (numb % 2 == 0){
+console.log("The number is even");
+}
+else{
+    console.log("The number is odd");
+}
+// Assignment 2
+let age = Number(prompt("Enter your age"));
+if (age >= 18){
+    console.log("You are signed in");
+}else{
+    console.log("Signup Failed")
+}
